a7a87c07b266e6454f4418d35de3c8f8972ccc25
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-11-15 14:12:43 +0530

    ADDED: update game status is completed, tie, who is winner

--- src/com/tictactoe/Board.java
+++ src/com/tictactoe/Board.java
@@ -11,6 +11,13 @@ public class Board {
 	private Player[] players;
 	// players identification letters for tic tac toe game according to index of players
 	private char[] playersIdentityLetters;
+	// sequences are mentioned for winning
+	private int[][] winSequences;
+	
+	private boolean isCompleted;
+	private boolean isTie;
+	private Player winner;
+	private boolean isStart;
 	
 	/**
 	 * creates board with 2 players
@@ -32,6 +39,36 @@ public class Board {
 		this.playersIdentityLetters[1] = 'X';
 		player1.setBoard(this);
 		player2.setBoard(this);
+		markWinningSequences();
+		isCompleted = false;
+		isTie = false;
+		winner = null;
+		isStart = false;
+	}
+	
+	private void markWinningSequences() {
+		winSequences = new int[8][3];
+		for(int i = 0; i < 8; i++) {
+			winSequences[i] = new int[3];
+		}
+		for(int i = 1; i < 8; i = i + 3) {
+			for(int j = i; j < i + 3; j++) {
+				winSequences[(int) (i / 3)][j - i] = j;
+			}
+		}
+		for(int i = 1; i < 4; i++) {
+			for(int j = i; j < i + 7; j = j + 3) {
+				winSequences[i + 2][(int) ((j - i) / 3)] = j;
+			}
+		}
+		
+		winSequences[6][0] = 1;
+		winSequences[6][1] = 5;
+		winSequences[6][2] = 9;
+		
+		winSequences[7][0] = 3;
+		winSequences[7][1] = 5;
+		winSequences[7][2] = 7;
 	}
 	
 	/**
@@ -165,19 +202,70 @@ public class Board {
 	}
 	
 	/*
+	 * check board information and set game status 
+	 * i.e. game completed or not if game completed then it is tie or not 
+	 * and if it is not tie then who is winner
+	 */
+	public void updateGameStatus() {
+		for(int i = 0; i < winSequences.length; i++) {
+			if(!Character.isLetter(boardArray[winSequences[i][0]])) {
+				continue;
+			}
+			char characterGot = boardArray[winSequences[i][0]];
+			boolean gotWinSequence = true;
+			for(int j = 1; j < winSequences[i].length; j++) {
+				if(Character.compare(characterGot, boardArray[winSequences[i][j]]) != 0) {
+					gotWinSequence = false;
+					break;
+				}
+			}
+			if(gotWinSequence) {
+				isCompleted = true;
+				isTie = false;
+				for(int playerIndex = 0; playerIndex < playersIdentityLetters.length; playerIndex++) {
+					if(Character.compare(characterGot, playersIdentityLetters[playerIndex]) == 0) {
+						winner = players[playerIndex];
+						break;
+					}
+				}
+				return;
+			}
+		}
+		for(int i = 1; i < boardArray.length; i++) {
+			if(!Character.isLetter(boardArray[i])) {
+				return;
+			}
+		}
+		isCompleted = true;
+		isTie = true;
+	}
+	
+	/*
 	 * board game will start here board will ask to random player to choose identity letter
 	 * from specified identity letter options
 	 */
 	public void play() {		
-		int playerIndex = toss();
-		System.out.println(players[playerIndex].getName()+" have won the toss and will play first");
-		setPlayerIdentityLetter(playerIndex, players[playerIndex].chooseIdentityLetter(playersIdentityLetters));
-		players[playerIndex].noteIdentityLetter(playersIdentityLetters[playerIndex]);
-		players[otherPlayerIndex(playerIndex)].noteIdentityLetter(playersIdentityLetters[otherPlayerIndex(playerIndex)]);
-		while(makeMove(players[playerIndex].makeMove(), playerIndex).equals(Move.INVALID));
-		playerIndex = otherPlayerIndex(playerIndex);
-		while(makeMove(players[playerIndex].makeMove(), playerIndex).equals(Move.INVALID));
-		showBoard();
+		for(int playerIndex = toss(); playerIndex < 2; playerIndex = otherPlayerIndex(playerIndex)) {
+			if(!isStart) {
+				System.out.println(players[playerIndex].getName()+" have won the toss and will play first");
+				setPlayerIdentityLetter(playerIndex, players[playerIndex].chooseIdentityLetter(playersIdentityLetters));
+				isStart = true;
+				players[playerIndex].noteIdentityLetter(playersIdentityLetters[playerIndex]);
+				players[otherPlayerIndex(playerIndex)].noteIdentityLetter(playersIdentityLetters[otherPlayerIndex(playerIndex)]);
+			}
+			while(makeMove(players[playerIndex].makeMove(), playerIndex).equals(Move.INVALID));
+			updateGameStatus();
+			if(isCompleted) {
+				break;
+			}
+		}
+		System.out.println("Tic Tac Toe game completed");
+		if(isTie) {
+			System.out.println("It is Tie");
+		} else {
+			System.out.println(winner.getName()+" is winner");
+		}
+		System.out.println(showCurrentBoard());
 	}
 	
 	public char[] getPlayerIdentityLetter() {
